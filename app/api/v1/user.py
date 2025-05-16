4308b25b77be05446aad12c87fb353cd83bfc232
seanlee0923 <leesinue0923@gmail.com>
2025-05-16 11:28:09 +0900

    db 세션 라우터에서 쓸수있게 함수 생성

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -1,12 +1,27 @@
-from fastapi import APIRouter
+import http
+
+from fastapi import APIRouter, HTTPException
+from sqlalchemy.orm import Session
+
+from app.schemas import user as schema
+
+from app.crud import user as crud
+from app.db.database import get_db
 
 router = APIRouter(
     prefix="/users",
     tags=["users"],
 )
+
+
 @router.post("/sign-in")
-def sign_in():
-    print("in")
+def sign_in(user: schema.UserCreate, db: Session = get_db):
+    existing_user = crud.get_user_by_username(db, user.username)
+    if existing_user:
+        raise HTTPException(status_code=http.HTTPStatus.CONFLICT, detail="Username already exists")
+
+    return crud.create_user(db,user)
+
 
 @router.get("/sign-up")
 def sign_up():
--- app/crud/user.py
+++ app/crud/user.py
@@ -5,6 +5,9 @@ from app.schemas.user import UserCreate
 def get_user(db: Session, user_id: int):
     return db.query(User).filter(User.id == user_id).first()
 
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
 def get_users(db: Session, skip: int = 0, limit: int = 30):
     return db.query(User).offset(skip).limit(limit).all()
 
