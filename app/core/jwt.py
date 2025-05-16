550228247e77c78a81f1cf27d4a4d19d65b63bc0
seanlee0923 <leesinue0923@gmail.com>
2025-05-16 13:08:16 +0900

    jwt 인증용 함수 생성

---
+++ app/core/jwt.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+from jose import JWSError, jwt
+from passlib.context import CryptContext
+
+SECRET_KEY = 'asdfsdfasdfsd'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_hashed_password(password):
+    return pwd_context.hash(password)
+
+def generate_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
